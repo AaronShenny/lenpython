9590dc805888b4d62fba30253d64278a2392ec9c
AaronShenny <aaronshenny102.go@gmail.com>
2024-01-09 18:02:08 +0530

    FINAL

---
+++ len.py
@@ -0,0 +1,22 @@
+def len3(value):
+    if isinstance(value,int):
+        num = 0
+        while value>0:
+            value = value//10
+            num = num + 1
+        
+    elif isinstance(value,str):
+        if hasattr(value, "__len__"):
+            return value.__len__()  # Calls the __len__() method of the object
+        else:
+            raise TypeError("Object of type {} has no len()".format(type(value)))
+    else:
+        if hasattr(value, "__len__"):
+            return value.__len__()  # Calls the __len__() method of the object
+        else:
+            raise TypeError("Object of type {} has no len()".format(type(value)))
+    return num
+
+value = input('Enter a value : ')   
+len1 = len(value)
+print(len1)
